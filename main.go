fa0c8b615997f04ce1d0d47c58ff0ecd14209316
CLorenzoH <christ.lorenzo.h@gmail.com>
2021-04-08 19:30:00 +0700

    endpoint articles created

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"Desc"`
+	Content string `json:"Content"`
+}
+
+type Articles []Article
+
+func all_articles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{
+			Title:   "title article",
+			Desc:    "description of the article",
+			Content: "content of the article",
+		},
+	}
+
+	fmt.Println("All articles endpoint hit")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func home_page(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Test homepage endpoint hit")
@@ -12,6 +34,7 @@ func home_page(w http.ResponseWriter, r *http.Request) {
 
 func handle_requests() {
 	http.HandleFunc("/", home_page)
+	http.HandleFunc("/articles", all_articles)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
